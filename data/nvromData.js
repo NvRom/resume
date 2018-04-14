849ef2a19a75d7a112a79c896e969aaf0ad28124
NvRom <sa515085@mail.ustc.edu.cn>
2018-04-14 13:09:36 +0800

    一些值得修改

--- data/nvromData.js
+++ data/nvromData.js
@@ -7,9 +7,9 @@ var userInfoList = [
 ];
 
 var userBackgroundList = [
-  { "infoName": "毕业院校", "infoVal": "中国科学技术大学软件学院" },
+  { "infoName": "毕业院校", "infoVal": "中国科学技术大学（USTC）" },
   { "infoName": "学历", "infoVal": "硕士" },
-  { "infoName": "毕业时间", "infoVal": "2018/12" },
+  { "infoName": "毕业时间", "infoVal": "2019/03" },
   { "infoName": "邮箱", "infoVal": "nvrom397@gmail.com" },
   { "infoName": "GitHub", "infoVal": "https://github.com/NvRom", "url": "url" },
   { "infoName": "求职意向", "infoVal": "C++开发/深度学习相关" }
@@ -20,13 +20,12 @@ var skillList = [
   { "skillName": "Data Structure", "percent": 80, "strokeWidth": "13", "color": "#1296db" },
   { "skillName": "Algorithm", "percent": 80, "strokeWidth": "13", "color": "#1296db" },
   { "skillName": "Machine Learning", "percent": 60, "strokeWidth": "13", "color": "#1296db" },
-  { "skillName": "Computer Vision", "percent": 70, "strokeWidth": "13", "color": "#1296db" },
-  { "skillName": "OpenCV", "percent": 60, "strokeWidth": "13", "color": "#1296db" },
-  { "skillName": "Linux", "percent": 30, "strokeWidth": "13", "color": "#1296db" },
+  { "skillName": "Computer Networks", "percent": 70, "strokeWidth": "13", "color": "#1296db" },
+  { "skillName": "Operating System", "percent": 70, "strokeWidth": "13", "color": "#1296db" },
   { "skillName": "Java", "percent": 30, "strokeWidth": "13", "color": "#1296db" },
   { "skillName": "JavaScript", "percent": 30, "strokeWidth": "13", "color": "#1296db" },
   { "skillName": "CSS/CSS3", "percent": 30, "strokeWidth": "13", "color": "#1296db" },
-  { "skillName": "SQL", "percent": 50, "strokeWidth": "13", "color": "#1296db" },
+  { "skillName": "DataBase", "percent": 50, "strokeWidth": "13", "color": "#1296db" },
 ];
 
 module.exports = {
